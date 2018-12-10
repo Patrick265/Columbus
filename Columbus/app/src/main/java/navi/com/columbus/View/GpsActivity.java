648f13854366602d7fe297a81d02fb5c17a0b489
Unknown <ketsh11@hotmail.com>
2018-12-10 14:15:57 +0100

    [Added] Google maps

--- Columbus/app/src/main/java/navi/com/columbus/View/GpsActivity.java
+++ Columbus/app/src/main/java/navi/com/columbus/View/GpsActivity.java
@@ -1,41 +1,153 @@
 package navi.com.columbus.View;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.AppComponentFactory;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import navi.com.columbus.R;
 
-public class GpsActivity extends AppCompatActivity implements OnMapReadyCallback
-{
+public class GpsActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
+    private SupportMapFragment mapView;
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
+    private Location mLastLocation;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.gps_map);
-        mapFragment.getMapAsync(this);
+
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        }
+
+
+        mapView = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map));
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap)
-    {
-        mMap = googleMap;
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
 
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        UiSettings settings = googleMap.getUiSettings();
+        settings.setZoomControlsEnabled(true);
+        settings.setTiltGesturesEnabled(true);
+        settings.setMapToolbarEnabled(false);
+        settings.setCompassEnabled(true);
+        mMap = googleMap;
+        mMap.setMinZoomPreference(12);
+        LatLng sydney = new LatLng(51.585843, 4.792213);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Avans Hogeschool Breda"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) return;
+        mMap.setMyLocationEnabled(true);
+        settings.setMyLocationButtonEnabled(true);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+        // use latitude and longitude given by
+        // location.getLatitude(), location.getLongitude()
+        // for updated location marker
+        Log.d("aaaaaaaa===>", "" + location.getLatitude() + "\n" + location.getLongitude());
+        // displayLocation();
+
+        // to remove old markers
+        mMap.clear();
+        final LatLng loc = new LatLng(location.getLongitude(), location.getLongitude());
+
+        Marker ham = mMap.addMarker(new MarkerOptions().position(loc).title("This is Me").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher_background)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, 15));
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
--- Columbus/app/src/main/java/navi/com/columbus/View/HomeActivity.java
+++ Columbus/app/src/main/java/navi/com/columbus/View/HomeActivity.java
@@ -1,7 +1,10 @@
 package navi.com.columbus.View;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import navi.com.columbus.R;
 
@@ -13,5 +16,14 @@ public class HomeActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), GpsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
