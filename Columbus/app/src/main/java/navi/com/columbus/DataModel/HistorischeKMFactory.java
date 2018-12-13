123de9d59aaa1f698934b8d62aaeb2aae90141c1
Marck420 <m.debont1@student.avans.nl>
2018-12-13 13:57:48 +0100

    [finished] loading in csv file historische km

--- Columbus/app/src/main/java/navi/com/columbus/DataModel/HistorischeKMFactory.java
+++ Columbus/app/src/main/java/navi/com/columbus/DataModel/HistorischeKMFactory.java
@@ -1,48 +1,44 @@
 package navi.com.columbus.DataModel;
 
-import android.content.res.AssetManager;
-import android.util.Log;
-
 import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderHeaderAware;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HistorischeKMFactory
 {
-    private Map<String, String> values;
+    private List<Monument> values;
 
     public HistorischeKMFactory()
     {
-        this.values = new HashMap<>();
+        this.values = new ArrayList<>();
     }
 
-    public int getHistKmMap()
+    public List<Monument> getHistKmMap()
     {
-        if(values == null)
+        if(values.isEmpty())
         {
             try
             {
-
-
                 CSVReader reader = new CSVReader(new FileReader("D:\\Documenten\\Technische Informatica\\2. Jaar 2\\2. Periode 2\\1. AGS A5\\4. Fase 3 - Implementatie\\Code\\Columbus\\Columbus\\app\\src\\main\\java\\navi\\com\\columbus\\Assets\\HistKm.csv"),';');
                 String[] nextLine;
                 while((nextLine = reader.readNext()) != null)
                 {
-                    System.out.println("HISTKM----"+ nextLine[0]);
-                    System.out.println("HISTKM----"+ nextLine[1]);
-                    System.out.println("HISTKM----"+ nextLine[2]);
-                    System.out.println("HISTKM----"+ nextLine[3]);
+                    Monument monument = new Monument.Builder().longitude(Double.valueOf(nextLine[1]))
+                            .latitude(Double.valueOf(nextLine[2]))
+                            .name(nextLine[3])
+                            .description(nextLine[4])
+                            .build();
+                    values.add(monument);
                 }
             } catch (IOException e)
             {
                 e.printStackTrace();
             }
         }
-        return 1;
+        return this.values;
     }
 
 }
