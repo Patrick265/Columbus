18d162c3b95acdf3193164705fdbbed5353e08e6
Patrick de Jong <patrick.dejong@protonmail.com>
2018-12-13 15:08:11 +0100

    [Added] retrieve for routes

--- Columbus/app/src/androidTest/java/navi/com/columbus/Service/DataStorageTest.java
+++ Columbus/app/src/androidTest/java/navi/com/columbus/Service/DataStorageTest.java
@@ -5,13 +5,14 @@ import android.support.test.InstrumentationRegistry;
 import org.junit.Test;
 
 import navi.com.columbus.DataModel.Monument;
+import navi.com.columbus.DataModel.Route;
 
 import static org.junit.Assert.*;
 
 public class DataStorageTest
 {
     private DataStorage storage = new DataStorage(InstrumentationRegistry.getTargetContext());
-    private  Monument monument = new Monument.Builder()
+    private Monument monument = new Monument.Builder()
             .name("TestNaam")
             .constructionYear(1995)
             .creator("CreatorName")
@@ -22,6 +23,13 @@ public class DataStorageTest
             .latitude(123123)
             .longitude(219312)
             .build();
+
+    private Route route = new Route.Builder()
+                    .name("Historische kilometer")
+                    .length(5)
+                    .description("Historische kilometer")
+                    .finished(true)
+                    .build();
 
     @Test
     public void onUpgrade()
@@ -61,6 +69,28 @@ public class DataStorageTest
         assertEquals(monument.getLatitude(), m.getLatitude(), 0.005);
         assertEquals(monument.getLongitude(), m.getLongitude(), 0.005);
         assertEquals(monument.getSoundURL(), m.getSoundURL());
+
+    }
+
+    @Test
+    public void addRoute()
+    {
+        this.storage.addRoute(route);
+    }
+
+    @Test
+    public void retrieveAllRoutes()
+    {
+        assertEquals(1, storage.retrieveAllRoutes().size());
+    }
+
+    @Test
+    public void retrieveRoute()
+    {
+        Route r = storage.retrieveRoute("Historische kilometer");
+        assertEquals(route.getName(), r.getName());
+        assertEquals(route.getDescription(), r.getDescription());
+        assertEquals(route.getLength(), r.getLength(), 0.005);
 
     }
 }
